041d572945ca5a5a32d8b3d0f9e1ad7b0f2a06d0
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-11-08 20:41:11 -0300

    jogo no arquivo principal

--- poker/main.py
+++ poker/main.py
@@ -10,41 +10,9 @@ deck.embaralhar()
 
 # Create players
 jogadores = []
-for nome in ['João', 'Maria', 'Pedro', 'Ana']:
-    jogadores.append(Jogador(nome))
+for nome in ['João', 'Maria', 'Pedro', 'Ana']: jogadores.append(Jogador(nome))
 
 # Create a new table
 mesa = Mesa(jogadores)
-
-
-# rodada de apostas
-
-# três cartas são colocadas na mesa
-for _ in range(3):
-    mesa.receber_carta(deck.pop())
-print(mesa)
-# rodada de apostas
-
-# uma carta é colocada na mesa
-mesa.receber_carta(deck.pop())
-
-# rodada de apostas
-
-# uma carta é colocada na mesa
-mesa.receber_carta(deck.pop())
-
-# rodada de apostas
-
-# jogadores mostram suas cartas
-
-# jogador com a melhor mão ganha
-
-# tudo é resetado
-
-
-## rodada de apostas
-# dealer paga a taxa inicial
-
-# próximo jogador escolhe entre correr, cobrir ou aumentar
-
-# se todos os jogadores cobrirem, a rodada termina
+print([jo.nome for jo in mesa.jogadores])
+mesa.jogo(deck)
